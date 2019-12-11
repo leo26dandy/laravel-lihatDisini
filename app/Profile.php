845ffcfed3504aaf76bb4a76e1ecdfa3df7115c5
dandy4satrio <dandy4satrio@gmail.com>
2019-12-12 01:41:42 +0700

    instagram clone

---
+++ app/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/QOJI2tHnX5gzplZrGweIYb7BBI064R2a7CvyXSM7.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
